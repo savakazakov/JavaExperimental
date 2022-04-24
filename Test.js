3f6940812f50bc34d26724bd66470dc9f56da1d9
savakazakov <savakazakov@gmail.com>
2022-04-24 21:49:43 +0100

    array functions

--- Test.js
+++ Test.js
@@ -1,26 +1,26 @@
 // Strict equality.
-console.log( "Strict equality:" );
-console.log( true === "true" );
-console.log( "0" === [] );
-console.log( [[]] === false );
-console.log( [1,[]] === false );
-console.log( true === true );
+console.log("Strict equality:");
+console.log(true === "true");
+console.log("0" === []);
+console.log([[]] === false);
+console.log([1, []] === false);
+console.log(true === true);
 // Non-strict equality.
-console.log( "Non-strict equality:" );
-console.log( true == true );
-console.log( true == "true" );
-console.log( "0" == [] );
-console.log( [[]] == false );
-console.log( [1,[]] == false );
-console.log( [] == [] );
-console.log( '5'== 5 );
+console.log("Non-strict equality:");
+console.log(true == true);
+console.log(true == "true");
+console.log("0" == []);
+console.log([[]] == false);
+console.log([1, []] == false);
+console.log([] == []);
+console.log('5' == 5);
 
 a = 4;
 a >>> 2;
-console.log(a<<1);
+console.log(a << 1);
 
 let someArray1 = [];
-console.log( someArray1[2] ) ; 
+console.log(someArray1[2]);
 
 let array = [];
 array[2] = 2;
@@ -36,7 +36,7 @@ console.log(array);
 let obj = { colour: "red", "long string": 100 };
 obj.length = 1.11;
 console.log(obj);
-console.log( obj.colour, obj["long string"] );
+console.log(obj.colour, obj["long string"]);
 
 console.log("///////////////////////////////////");
 
@@ -46,65 +46,79 @@ console.log(str);
 str.year = 2;
 console.log(str.year);
 
-console.log( "test", Object.getOwnPropertyDescriptor(obj, "colour") );
-Object.defineProperty(obj, "colour", {writable: true, enumerable: false, configurable: true});
+console.log("test", Object.getOwnPropertyDescriptor(obj, "colour"));
+Object.defineProperty(obj, "colour", { writable: true, enumerable: false, configurable: true });
 console.log(obj);
-Object.defineProperty(obj, "colour", {configurable: true});
+Object.defineProperty(obj, "colour", { configurable: true });
 console.log(obj);
 
 let book =
 {
     title: 'some title',
     author: 'author name',
-    info: function(a)
-    {
+    info: function (a) {
         return `${a} ${this.title} written by ${this.author}`;
     }
 };
 console.log(book.info(2));
 
-let arr = [1,2,3,4];
+let arr = [1, 2, 3, 4];
 console.log(Object.keys(arr));
 
-function addDef( a, b, c = a + b )
-{
+function addDef(a, b, c = a + b) {
     return a + b + c;
 }
 
-console.log( addDef( 1, 2 ) );
+console.log(addDef(1, 2));
 
-console.log( addDef( 1, undefined ) );
+console.log(addDef(1, undefined));
 
-function noArgs()
-{ 
-    console.log( arguments ) ;
+function noArgs() {
+    console.log(arguments);
 }
 
-noArgs( 1 ) ; // [Arguments] { '0' : 1 } 
-noArgs( 1, 2, 3 ); // [Arguments] { '0': 1, '1': 2, '2': 3 }
-noArgs( {"complex": "types"} ); // [Arguments] { '0': { complex: 'types' } }
+noArgs(1); // [Arguments] { '0' : 1 } 
+noArgs(1, 2, 3); // [Arguments] { '0': 1, '1': 2, '2': 3 }
+noArgs({ "complex": "types" }); // [Arguments] { '0': { complex: 'types' } }
 
-function cannotDO()
-{ 
-    console.log( arguments. shift() ) ;
+function cannotDO() {
+    console.log(arguments.shift());
 }
 
-( {a, d} = { a: "thing", b: 1000, c:69 } )
+({ a, d } = { a: "thing", b: 1000, c: 69 })
 console.log(a, d);
 
 
-function complexFunc2( a, b = 10, c = 20, ...d )
-{
+function complexFunc2(a, b = 10, c = 20, ...d) {
     console.log(a, b, c, d);
 };
 
 let array3 = [1, 2, 3, 4, 6];
-complexFunc2( ...array3 );
+complexFunc2(...array3);
 
 const data = [1, 2, 3, 4, 5]
-const newData = data.map( 
+const newData = data.map(
     (elem, index, arr) => elem * 2
 );
 
 console.log(data);
 console.log(newData);
+
+let testFunc = complexFunc2;
+console.log("function pointer");
+testFunc(1, 2)
+
+console.log("scope");
+
+function outerFunc() {
+    let a = 10;
+    console.log(a);
+    function innerFunc() {
+        let b = 29;
+        console.log(a);
+    }
+    return innerFunc;
+}
+
+let f = outerFunc();
+f();
